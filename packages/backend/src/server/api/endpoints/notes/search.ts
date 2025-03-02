d1b9365e886375ac4b7a15a41835c5313ded19cf
乃苺佳寿 <chikorita157@chikorita157.com>
2025-03-01 20:24:20 -0500

    Disable filetypecategories as its not supported under ElasticSearch

--- packages/backend/src/server/api/endpoints/notes/search.ts
+++ packages/backend/src/server/api/endpoints/notes/search.ts
@@ -5,7 +5,7 @@
 
 import { Injectable } from '@nestjs/common';
 import { Endpoint } from '@/server/api/endpoint-base.js';
-import { fileTypeCategories, SearchService } from '@/core/SearchService.js';
+import { SearchService } from '@/core/SearchService.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
 import { RoleService } from '@/core/RoleService.js';
 import { ApiError } from '../../error.js';
@@ -52,11 +52,11 @@ export const paramDef = {
 			type: 'string',
 			description: 'The local host is represented with `.`.',
 		},
-		filetype: {
+		/*filetype: {
 			type: 'string',
 			nullable: true,
 			enum: fileTypeCategories,
-		},
+		},*/
 		userId: { type: 'string', format: 'misskey:id', nullable: true, default: null },
 		channelId: { type: 'string', format: 'misskey:id', nullable: true, default: null },
 		order: { type: 'string' },
